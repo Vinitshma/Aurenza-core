a280f5bd17b6dfd4298fcac0f8be1fcc70b522c8
Vinit kumar sharma <96139405+Vinitshma@users.noreply.github.com>
2025-11-26 19:09:15 +0530

    LCL-1 Folder Re-structured (#9)

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/home.module.css";
+
+export default function HomePage(){
+    return (
+        <div className={styles.home}>
+            <h1>Hello home</h1>
+        </div>
+    );
+}
